958dff7044730b3980fa06030504e5b71e8ea622
Aryan Agrawal <61613340+shad0waryan@users.noreply.github.com>
2023-02-03 14:23:49 +0530

    commit2

--- src/logout.js
+++ src/logout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./login.css";
-import { Link, Navigate } from "react-router-dom";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Mycartcontext } from "./context";
 import { useContext } from "react";
 
